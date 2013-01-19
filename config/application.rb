87157210f1d8705a6f72825f4067a0fa4234c23f
João Figueiredo <lucalanca@hotmail.com>
2013-01-19 23:05:25 +0000

    Trying to fix compass on heroku.

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,7 @@ module OQueFaltaNoPorto
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
 
     config.generators do |g|
         g.fixture_replacement :factory_girl
